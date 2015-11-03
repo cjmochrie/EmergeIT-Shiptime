32dbd29e136e62e77605bb5334e367d735e71eb4
Cameron Mochrie <cjmochrie@users.noreply.github.com>
2015-11-02 21:41:22 -0500

    Update shiptime.py
    Minor formatting

--- shiptime.py
+++ shiptime.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as ET
 import logging
 import re
-
-import requests
-import xmltodict
 from decimal import *
 from operator import itemgetter
 
-logging.basicConfig()  # you need to initialize logging, otherwise you will not see anything from requests
+import requests
+import xmltodict
+
+
+logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
@@ -20,9 +21,10 @@ class Shiptime:
     http://sandbox.shiptime.com/api/rating.html#getRates
     """
     def __init__(self, encrypted_password='2A27C164EA2499506FB0CD628AB7B083',
-                 encrypted_username='20AE2D8B660FB10B1F4625BB77FCAE3C3EA33586F2DDA2A1EB3349881952AC23', sandbox=True,
-                 street_address2=None, notify=False, instructions=None, residential=False, email=None, *, country,
-                 postal_code, province, attention, city, company_name, phone, street_address):
+                 encrypted_username='20AE2D8B660FB10B1F4625BB77FCAE3C3EA33586F2DDA2A1EB3349881952AC23',
+                 sandbox=True, street_address2=None, notify=False, instructions=None,
+                 residential=False, email=None, *, country, postal_code, province,
+                 attention, city, company_name, phone, street_address):
 
         """Instance is initialized along with sender information"""
         """Default username/password for the sanbbox server"""
@@ -50,10 +52,12 @@ class Shiptime:
         self.instructions = instructions
         self.street_address2 = street_address2
 
-    def get_rates(self, residential=False, notify=False, email=None, instructions=None, street_address2=None,
-                  signature=None, saturday_service=None, retry=False, *, items, package_type, country, postal_code,
+    def get_rates(self, residential=False, notify=False, email=None, instructions=None,
+                  street_address2=None, signature=None, saturday_service=None,
+                  retry=False, *, items, package_type, country, postal_code,
                   province, attention, city, company_name, phone, street_address):
-        """Requires a list of dictionaries for items_tag (height, length, width, weight in Inches and lbs respectively
+        """Requires a list of dictionaries for items_tag
+        (height, length, width, weight in Inches and lbs respectively
         Returns a dictionary of two lists:
         'messages'
         'rates' - sorted ascending by total_before_tax
@@ -63,9 +67,11 @@ class Shiptime:
             raise TypeError('Items dictionary required')
 
         # Build the XML Request
-        root = ET.Element('S:Envelope', attrib={'xmlns:S': 'http://schemas.xmlsoap.org/soap/envelope/'})
+        root = ET.Element('S:Envelope',
+                          attrib={'xmlns:S': 'http://schemas.xmlsoap.org/soap/envelope/'})
         body = ET.SubElement(root, 'S:Body')
-        rates = ET.SubElement(body, 'ns2:getRates', attrib={'xmlns:ns2': 'http://v1.api.emergeit.com/'})
+        rates = ET.SubElement(body, 'ns2:getRates',
+                              attrib={'xmlns:ns2': 'http://v1.api.emergeit.com/'})
         key = ET.SubElement(rates, 'Key')
         ET.SubElement(key, 'EncryptedPassword').text = self.encrypted_password
         ET.SubElement(key, 'EncryptedUsername').text = self.encrypted_username
@@ -95,26 +101,29 @@ class Shiptime:
             try:
                 height = ET.SubElement(item_tag, 'Height')
                 ET.SubElement(height, 'UnitsType').text = 'IN'
-                ET.SubElement(height, 'Value').text = str(round(Decimal(item['height']) + Decimal(
-                    0.5), 0))
+                ET.SubElement(height, 'Value').text = str(round(Decimal(item['height']) +
+                                                                Decimal(0.5), 0))
             except KeyError:
                 raise KeyError('Key Error on height')
             try:
                 length = ET.SubElement(item_tag, 'Length')
                 ET.SubElement(length, 'UnitsType').text = 'IN'
-                ET.SubElement(length, 'Value').text = str(round(Decimal(item['length']) + Decimal(0.5), 0))
+                ET.SubElement(length, 'Value').text = str(round(Decimal(item['length']) +
+                                                                Decimal(0.5), 0))
             except KeyError:
                 raise KeyError('Key Error on length')
             try:
                 width = ET.SubElement(item_tag, 'Width')
                 ET.SubElement(width, 'UnitsType').text = 'IN'
-                ET.SubElement(width, 'Value').text = str(round(Decimal(item['width']) + Decimal(0.5), 0))
+                ET.SubElement(width, 'Value').text = str(round(Decimal(item['width']) +
+                                                               Decimal(0.5), 0))
             except KeyError:
                 raise KeyError('Key Error on width')
             try:
                 weight = ET.SubElement(item_tag, 'Weight')
                 ET.SubElement(weight, 'UnitsType').text = 'LB'
-                ET.SubElement(weight, 'Value').text = str(round(Decimal(item['weight']) + Decimal(0.5), 0))
+                ET.SubElement(weight, 'Value').text = str(round(Decimal(item['weight']) +
+                                                                Decimal(0.5), 0))
             except KeyError:
                 raise KeyError('Key Error on weight')
 
@@ -158,7 +167,8 @@ class Shiptime:
                       "http://v1.api.emergeit.com/": None}
 
         # Parse the response into a dictionary
-        response_dict = xmltodict.parse(response.text, process_namespaces=True, namespaces=namespaces)
+        response_dict = xmltodict.parse(response.text, process_namespaces=True,
+                                        namespaces=namespaces)
 
         # Dictionary to hold the processed results
         shiptime_response = {'messages': [], 'rates': []}
@@ -167,16 +177,18 @@ class Shiptime:
         messages = response_dict['Envelope']['Body']['getRatesResponse']['Response']['Messages']
 
         if messages:
-            # Because of the way the xml is parsed need to treat singleton messages differently
-            # Essentially a single message is a dict inside a dict, whereas multiple messages are dicts inside lists
-            # inside dicts
+            # Because of the way the xml is parsed need to treat singleton messages
+            # differently Essentially a single message is a dict inside a dict,
+            # whereas multiple messages are dicts inside lists # inside dicts
             if not isinstance(messages['Message'], list):
-                shiptime_response['messages'].append({'severity': messages['Message']['Severity'],
-                                                      'text': messages['Message']['Text']})
+                shiptime_response['messages'].\
+                    append({'severity': messages['Message']['Severity'],
+                            'text': messages['Message']['Text']})
             else:
                 for message in messages['Message']:
                     print(message)
-                    shiptime_response['messages'].append({'severity': message['Severity'], 'text': message['Text']})
+                    shiptime_response['messages'].\
+                        append({'severity': message['Severity'], 'text': message['Text']})
 
         # Do a check to see if there was an error about postal codes.
         # If there was (and this is not a retry), redo the request with the suggested city
@@ -187,17 +199,21 @@ class Shiptime:
                     corrected_city = re.findall(r"only valid for ([^,]*)", message['text'])[0]
                     print('Retrying shiptime API. Replacing {} with {}'.format(city, corrected_city))
 
-                    return self.get_rates(residential=residential, notify=notify, email=email,
-                                          instructions=instructions, street_address2=street_address2,
-                                          signature=signature, saturday_service=saturday_service, retry=True,
-                                          items=items, package_type=package_type, country=country,
-                                          postal_code=postal_code, province=province, attention=attention,
-                                          city=corrected_city, company_name=company_name, phone=phone,
-                                          street_address=street_address)
+                    return self.\
+                        get_rates(residential=residential, notify=notify, email=email,
+                                  instructions=instructions,
+                                  street_address2=street_address2, signature=signature,
+                                  saturday_service=saturday_service, retry=True,
+                                  items=items, package_type=package_type, country=country,
+                                  postal_code=postal_code, province=province,
+                                  attention=attention, city=corrected_city,
+                                  company_name=company_name, phone=phone,
+                                  street_address=street_address)
         else:
             # Add a message that the city was modified (only reason we are here is this is a 'retry'
-            shiptime_response['messages'].append({'severity': 'Warning',
-                                                  'text': 'City was changed to {}.'.format(city)})
+            shiptime_response['messages'].\
+                append({'severity': 'Warning',
+                        'text': 'City was changed to {}.'.format(city)})
 
         # Bail out if an error message is returned
         for message in shiptime_response['messages']:
@@ -210,16 +226,17 @@ class Shiptime:
         if available_rates:
             # Same reasoning per the above
             if not isinstance(available_rates['Rate'], list):
-                shiptime_response['rates'].append({'service_name': available_rates['Rate']['ServiceName'],
-                                        'total_before_tax': Decimal(available_rates['Rate']['TotalBeforeTaxes']['Amount'])/100,
-                                        'transit_time': int(available_rates['Rate']['TransitDays'])})
+                shiptime_response['rates'].\
+                    append({'service_name': available_rates['Rate']['ServiceName'],
+                            'total_before_tax': Decimal(available_rates['Rate']['TotalBeforeTaxes']['Amount'])/100,
+                            'transit_time': int(available_rates['Rate']['TransitDays'])})
 
             else:
                 for rate in available_rates['Rate']:
-                    shiptime_response['rates'].append({'service_name': rate['ServiceName'],
-                                                       'total_before_tax': Decimal(rate['TotalBeforeTaxes'][
-                                                                                       'Amount'])/100,
-                                                       'transit_time': int(rate['TransitDays'])})
+                    shiptime_response['rates'].\
+                        append({'service_name': rate['ServiceName'],
+                                'total_before_tax': Decimal(rate['TotalBeforeTaxes']['Amount'])/100,
+                                'transit_time': int(rate['TransitDays'])})
 
             # Sort the rates ascending
             shiptime_response['rates'].sort(key=itemgetter('total_before_tax'))
@@ -231,14 +248,18 @@ class Shiptime:
 # http://sandbox.shiptime.com/api/SampleGetRatesRequest.xml
 if __name__ == '__main__':
 
-    shipper = Shiptime(country='CA', postal_code='M9A4M5', province='ON', attention='Attention name', city='Etobicoke',
-                       company_name='Origin Company', phone='123-456-7890', street_address='Address 1')
+    shipper = Shiptime(country='CA', postal_code='M9A4M5', province='ON',
+                       attention='Attention name', city='Etobicoke',
+                       company_name='Origin Company', phone='123-456-7890',
+                       street_address='Address 1')
 
     response = shipper.get_rates(items=[{'height': 3, 'length': 5, 'width': 4, 'weight': 2},
                                         {'height': 3, 'length': 5, 'width': 4, 'weight': 2}],
-                                 package_type='PACKAGE', country='CA', postal_code='M2M2M2', residential=False,
-                                 province='ON', attention='Attention name', city='North York',
-                                 company_name='Test Company', phone='123-456-7890', street_address='Address 1')
+                                 package_type='PACKAGE', country='CA',
+                                 postal_code='M2M2M2', residential=False, province='ON',
+                                 attention='Attention name', city='North York',
+                                 company_name='Test Company', phone='123-456-7890',
+                                 street_address='Address 1')
 
     for message in response['messages']:
         print(message)
